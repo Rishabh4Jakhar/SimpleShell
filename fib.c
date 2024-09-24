778bee0af96eb6ed95b87bb68769713ba71e1b6e
UdayPandita <162724237+UdayPandita@users.noreply.github.com>
2024-09-24 13:59:09 +0530

    Create fib.c

--- fib.c
+++ fib.c
@@ -1,1 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+int fib(int n) {
+  if (n < 2) {
+    return n;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  }
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("Usage: %s <n>\n", argv[0]);
+    return 1;
+  }
+  int n = atoi(argv[1]);
+  int val = fib(n);
+  printf("Fib(%d) = %d\n", n, val);
+  return 0;
+}
