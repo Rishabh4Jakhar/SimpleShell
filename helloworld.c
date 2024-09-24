682e0d601cf24848f00aae92a07348aa73d4df04
Rishabh Jakhar <75088570+Rishabh4Jakhar@users.noreply.github.com>
2024-09-24 13:53:16 +0530

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello ");
+    printf("World");
+    printf("!\n");
+    printf("Goodbye, World!\n");
+    printf("Need to study for OS exam\n");
+    return 0;
+}
